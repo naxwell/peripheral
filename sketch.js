96cd607187c31d120367419664bb000d6f11adaf
naxwell <maxwell.lander@gmail.com>
2017-10-31 20:05:12 -0400

    Add files via upload
    blank p5

---
+++ sketch.js
@@ -0,0 +1,21 @@
+
+
+function setup() 
+{
+  createCanvas(windowWidth, windowHeight);
+  stroke(255);
+  noFill();
+}
+
+function draw() 
+{
+
+
+}
+
+
+
+function windowResized() 
+{
+  resizeCanvas(windowWidth, windowHeight);
+}
